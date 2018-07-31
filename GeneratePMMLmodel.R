428f65dc4cce7c35225a5661e2bc290f05200474
Cederic Bosmans <bosmansc6@gmail.com>
2018-07-31 04:03:12 +0200

    Add files via upload

--- GeneratePMMLmodel.R
+++ GeneratePMMLmodel.R
@@ -8,6 +8,7 @@ if(!require(chron)) { install.packages("chron"); library(chron); }
 if(!require(plyr)) { install.packages("plyr"); library(plyr); }
 if(!require(dplyr)) { install.packages("dplyr"); library(dplyr); }
 if(!require(plotly)) { install.packages("plotly"); library(plotly); }
+if(!require(BBmisc )) { install.packages("BBmisc"); library(plotly); }
 
 
 #mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/AAPL_batch_big_R.csv")  # read csv file 
@@ -19,15 +20,18 @@ if(!require(plotly)) { install.packages("plotly"); library(plotly); }
 #mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/JNJ_Big_0.2.csv")  # read csv file 
 #mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/JNJ_Big_0.5.csv")  # read csv file 
 #mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/ORCL_big.csv")  # read csv file 
-#mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/MS_big.csv")  # read csv file 
-mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/SO_big.csv")  # read csv file 
+mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/MS_big.csv")  # read csv file 
+#mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/SO_big.csv")  # read csv file 
 #mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/JNJ_big.csv")  # read csv file 
 #mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/MS_big_period.csv")  # read csv file
+#mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/SO_big2.csv")  # read csv file
+#mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/GOOGL_big.csv")  # read csv file
+#mydata <- read.csv("C:/Users/ceder/Flink/BatchStockData/batchData/C_big.CSV")  # read csv file
 
 ## delete first 100 rows bc variables are not correctly calculated by then
 mydata <- mydata[-c(1:100),]
 
-colnames(mydata) <- c("dateTime", "name", "lastPrice", "lastPriceLag", "SMA10", "SMA100", "SMA_signal", "SMA_direction","BB_lowerbound", "BB_upperbound", "Bollinger Band", "BB_signal", "BB_direction","CCI", "CCI_signal", "CCI_direction",
+colnames(mydata) <- c("dateTime", "name", "lastPrice", "lastPriceLag", "SMA10", "SMA100", "SMA_signal", "SMA_direction","BB_lowerbound", "BB_upperbound", "BB_middleband", "BB_signal", "BB_direction","CCI", "CCI_signal", "CCI_direction",
                     "stoch",  "stoch_signal", "stoch_direction","RSI", "RSI_signal", "RSI_direction","MFI", "MFI_signal", "moneyFlowIndex_direction",
                     "chaikin",  "chaikin_signal", "chaikin_direction","willR", "willR_signal", "williamsR_direction")
 mydata[,1] <- substr(mydata[,1], 0,19)
@@ -105,7 +109,7 @@ returnTable <- basetable[1:52512, c("dateTime", "lastPrice.x", "lastPrice.y", "r
 
 
 returnTable$prediction <- returnTable$responseVariable
-vreturnTable <- na.omit(returnTable)
+returnTable <- na.omit(returnTable)
 
 ## make extra dateTime column for end of day selling
 returnTable$dateTime <- factor(returnTable$dateTime )
@@ -218,10 +222,10 @@ for (k in deploymentPeriods){
   
     for(i in trainingPeriods){
       
-    duration <- week
-    deploymentPeriod <- day
+    duration <- 6000
+    deploymentPeriod <- 5000
     durationTest <- duration + deploymentPeriod
-    threshold <- 0.01
+    threshold <- 0.2
     
     ## define response variable based on threshold
     basetable$responseVariable <- factor(ifelse(basetable$lastPrice.x - basetable$lastPrice.y >= threshold , 2 , ifelse(basetable$lastPrice.y - basetable$lastPrice.x >= threshold , 1 , 0)))
@@ -240,9 +244,9 @@ for (k in deploymentPeriods){
     ## make prediction and prediction column
     #RandomForest <-randomForest(responseVariable~.,data=basetableTrain2[, c(2,3,8,11,14,17,20,23,26,29)], ntree=100) 
     rf <-randomForest(responseVariable~.,data=basetableTrain2, ntree=100) 
-    varImpPlot(rf,type=2)
+    varImpPlot(rf,type=2, n.var = 12, main = "Random Forest")
     
-    confusionMatrix(predict(rf,newdata=basetableTest2) , returnTable$responseVariable)
+    confusionMatrix(predict(rf, newdata=basetableTest2) , returnTable$responseVariable)
     
     returnTable$prediction <- predict(rf,newdata=basetableTest2) 
     (result <- confusionMatrix(returnTable$prediction, returnTable$responseVariable))
@@ -258,7 +262,8 @@ for (k in deploymentPeriods){
     returnTable$dateTime <- factor(returnTable$dateTime )
     returnTable$dateTimeHead <- lead(returnTable$dateTime)
     
-    returnTable$predictionDay <- as.numeric(ifelse(returnTable$dateTime != returnTable$dateTimeHead, 3, returnTable$prediction))
+  #  returnTable$predictionDay <- as.numeric(ifelse(returnTable$dateTime != returnTable$dateTimeHead, 3, returnTable$prediction))
+    returnTable$predictionDay <- as.numeric(returnTable$prediction)
     
     
     ## remove hold rows except end of day
@@ -511,7 +516,7 @@ for (k in deploymentPeriods){
   
     ## remove for next loop
     rm(baseTable)
-    rm(returnTable)
+   # rm(returnTable)
     rm(basetableTrain2)
     rm(basetableTest2)
   
@@ -523,10 +528,15 @@ for (k in deploymentPeriods){
 
 
 resultDF <- na.omit(resultDF)
+resultDF1 <- na.omit(resultDF)
 resultDFweek <- na.omit(resultDFweek)
+resultDFweek1 <- na.omit(resultDFweek)
 resultDFtwoweek <- na.omit(resultDFtwoweek)
+resultDFtwoweek1 <- na.omit(resultDFtwoweek)
 month <- na.omit(month)
+month1 <- na.omit(month)
 twomonth <- na.omit(twomonth)
+twomonth1 <- na.omit(twomonth)
 
 sum(resultDF$Return)
 max(resultDF$Return)
@@ -569,40 +579,59 @@ y <- list(
 
 Period <- resultDF$Period/389
 
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 
-plot_ly( data = resultDF, x = Period, y = ~Return2, type = "scatter", mode = "markers", marker = list(size = 7), name = 'Treshold 0.10') %>%
+resultDFplot <- resultDF[,c(3,6)]/(max(resultDF$Transactions))*30
+
+resultDFweekplot <-resultDFweek[,c(3,6,9)]/(max(resultDFweek$Transactions))*30
+resultDFweekplot$Transactions3 <-resultDFweek[,c(9)]/(max(resultDFweek$Transactions))*50
+
+resultDFtwoweekplot <-resultDFtwoweek[,c(3,6)]/(max(resultDFtwoweek$Transactions))*30
+
+monthplot <-month[,c(3,6)]/(max(month$Transactions))*40
+
+twomonthplot<-twomonth[,c(3,6)]/(max(twomonth$Transactions))*30
+
+
+plot_ly( data = resultDF, x = Period, y = ~Return2, type = "scatter", mode = "markers", marker = list(size = resultDFplot$Transactions), name = 'Treshold 0.01') %>%
   layout(xaxis = x, yaxis = y ,title = 'One day deployment period')%>% 
-  add_trace(data = resultDF, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.01')%>% 
-  add_trace(data = resultDF, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.20')%>% 
-  add_trace(data = resultDF, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.50')
+  add_trace(data = resultDF, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = resultDFplot$Transactions2),  name = 'Treshold 0.10')%>% 
+  add_trace(data = resultDF, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = resultDFplot$Transactions3),  name = 'Treshold 0.20')%>% 
+  add_trace(data = resultDF, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = resultDFplot$Transactions4),  name = 'Treshold 0.50')
 
 
-plot_ly( data = resultDFweek, x = Period, y = ~Return2, type = "scatter", mode = "markers", marker = list(size = 7), name = 'Treshold 0.10') %>%
+plot_ly( data = resultDFweek, x = Period, y = ~Return2, type = "scatter", mode = "markers", marker = list(size = resultDFweekplot$Transactions), name = 'Treshold 0.01') %>%
   layout(xaxis = x, yaxis = y, title = 'One week deployment period')%>% 
-  add_trace(data = resultDFweek, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.01')%>% 
-  add_trace(data = resultDFweek, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = 7),name = 'Treshold 0.20')%>% 
-  add_trace(data = resultDFweek, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.50')
+  add_trace(data = resultDFweek, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = resultDFweekplot$Transactions2),  name = 'Treshold 0.10')%>% 
+  add_trace(data = resultDFweek, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = resultDFweekplot$Transactions3),name = 'Treshold 0.20')%>% 
+  add_trace(data = resultDFweek, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = resultDFweekplot$Transactions4),  name = 'Treshold 0.50')
 
 
-plot_ly( data = resultDFtwoweek, x = Period, y = ~Return2, type = "scatter", mode = "markers", marker = list(size = 7), name = 'Treshold 0.10') %>%
+plot_ly( data = resultDFtwoweek, x = Period, y = ~Return2, type = "scatter", mode = "markers", marker = list(size = resultDFtwoweekplot$Transactions), name = 'Treshold 0.01') %>%
   layout(xaxis = x, yaxis = y,title = 'two weeks deployment period')%>% 
-  add_trace(data = resultDFtwoweek, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.01')%>% 
-  add_trace(data = resultDFtwoweek, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = 7),name = 'Treshold 0.20')%>% 
-  add_trace(data = resultDFtwoweek, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.50')
+  add_trace(data = resultDFtwoweek, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = resultDFtwoweekplot$Transactions2),  name = 'Treshold 0.10')%>% 
+  add_trace(data = resultDFtwoweek, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = resultDFtwoweekplot$Transactions3),name = 'Treshold 0.20')%>% 
+  add_trace(data = resultDFtwoweek, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = resultDFtwoweekplot$Transactions4),  name = 'Treshold 0.50')
 
-plot_ly( data = month, x = Period, y = ~Return2, type = "scatter", mode = "markers", marker = list(size = 7), name = 'Treshold 0.10') %>%
+plot_ly( data = month, x = Period, y = ~Return2, type = "scatter", mode = "markers", marker = list(size =monthplot$Transactions), name = 'Treshold 0.01') %>%
   layout(xaxis = x, yaxis = y,title = 'One month deployment period')%>% 
-  add_trace(data = month, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.01')%>% 
-  add_trace(data = month, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = 7),name = 'Treshold 0.20')%>% 
-  add_trace(data = month, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.50')
+  add_trace(data = month, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = monthplot$Transactions2),  name = 'Treshold 0.10')%>% 
+  add_trace(data = month, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = monthplot$Transactions3),name = 'Treshold 0.20')%>% 
+  add_trace(data = month, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = monthplot$Transactions4),  name = 'Treshold 0.50')
 
-plot_ly( data = twomonth, x = Period, y = ~Return2, type = "scatter", mode = "markers", marker = list(size = 7), name = 'Treshold 0.10') %>%
+plot_ly( data = twomonth, x = Period, y = ~Return2, type = "scatter", mode = "markers", name = 'Treshold 0.01', marker = list(size = twomonthplot$Transactions, opacity = 0.8)) %>%
   layout(xaxis = x, yaxis = y,title = 'two months deployment period')%>% 
-  add_trace(data = twomonth, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.01')%>% 
-  add_trace(data = twomonth, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = 7),name = 'Treshold 0.20')%>% 
-  add_trace(data = twomonth, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = 7),  name = 'Treshold 0.50')
+  add_trace(data = twomonth, x = Period, y = ~Return, type = "scatter", mode = "markers", marker = list(size = twomonthplot$Transactions2),  name = 'Treshold 0.10')%>% 
+  add_trace(data = twomonth, x = Period, y = ~Return3, type = "scatter", mode = "markers", marker = list(size = twomonthplot$Transactions3),name = 'Treshold 0.20')%>% 
+  add_trace(data = twomonth, x = Period, y = ~Return4, type = "scatter", mode = "markers", marker = list(size = twomonthplot$Transactions4),  name = 'Treshold 0.50')
 
+save(resultDF,file="resultDFms.Rda")
+save(resultDFweek,file="resultDFweekms.Rda")
+save(resultDFtwoweek,file="resultDFtwoweekms.Rda")
+save(month,file="monthms.Rda")
+save(twomonth,file="twomonthms.Rda")
 
+load()
 
 
 
